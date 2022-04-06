3cb6e75f5b33d8c75cef3b6ef516ec4ef54c8a0e
brembilla28 <50902847+brembilla28@users.noreply.github.com>
2022-04-06 15:08:30 +0200

    fix refuso

--- src/classi/Brick.java
+++ src/classi/Brick.java
@@ -1,54 +1,51 @@
 package classi;
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
 public class Brick {
 	
     int x,y, hp;
-    String powerup;
     static Image[] brickIcons;		
 
     
-    public Brick(int x, int y, int hp, String powerup){
+    public Brick(int x, int y, int hp){
 		
         this.x = x;
         this.y = y;
         this.hp = hp;
-        this.powerup = powerup;
         
-        brickIcons = new Image[3];
+        brickIcons = new Image[4];
         
         for(int i=0; i<3; i++){
         	brickIcons[i] = new ImageIcon("brick"+Integer.toString(i+1)+".png").getImage();		
         }
     }
 	
+	//ottenimento immagine mattoncino in base a hp
 	public Image getImg(){
 		return brickIcons[hp-1];
 	}
 	
+    //ottenimento hp
     public int getHP() {
         return hp;
     }
     
-    public String getPU(){
-    	return powerup;
-    }
  
+    //ottenimento coordinata x
     public int getX() {
         return x;
     }
 
+    //ottenimento coordinata y
     public int getY() {
         return y;
     }
     
+    //impostazione hp
     public void setHP(int i) {
         hp = i;
-    }
+    }}
     
-    public void setPU(String pu){
-    	powerup = pu;
-    }
 
-}
