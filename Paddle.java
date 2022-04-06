661999f4d02358959548e9b80b523e58a3bf11b7
brembilla28 <50902847+brembilla28@users.noreply.github.com>
2022-04-06 02:44:18 -0700

    update brick e paddle

---
+++ Paddle.java
@@ -0,0 +1,69 @@
+package classi;
+
+import java.awt.event.*;
+
+
+
+
+public class Paddle{
+	
+	
+	private int x, y, width; 		
+
+	
+	public Paddle(int x, int y){
+		
+		
+		this.x = x;
+		this.y = y;
+		width = 60;
+	}
+	
+	public void move(boolean[] keys){
+		
+		
+		if(keys[KeyEvent.VK_LEFT]){
+ 			if(this.getX() > 0) {
+                this.setX(this.getX() - 5);
+ 			}
+        }
+        if(keys[KeyEvent.VK_RIGHT]){
+        	if(this.getX() + 60 < 500) {
+            	this.setX(this.getX() + 5);
+            }
+        }
+
+	}
+	
+
+	
+	public int getWidth(){
+		return width;
+	}
+	
+	
+	public int getX(){
+		return x;
+	}
+	
+	
+	public int getY(){
+		return y;
+	}
+	
+	
+	public void setWidth(int i){
+		width = i;
+	}
+	
+	
+	public void setX(int i){
+		x = i;
+	}
+	
+	public void setY(int i){
+		y = i;
+	}
+	
+	
+}
