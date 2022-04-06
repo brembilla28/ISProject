17641690e4270d7b92f301c9110e008e8fa27997
brembilla28 <50902847+brembilla28@users.noreply.github.com>
2022-04-06 11:55:29 +0200

    creation mybutton

---
+++ src/classi/MyButton.java
@@ -0,0 +1,22 @@
+package classi;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
+class MyButton extends JButton{
+	
+	//Custom JButton class
+
+	public MyButton(String f){
+		
+		ImageIcon p = new ImageIcon(f+"P.png");			
+		ImageIcon d = new ImageIcon(f+"D.png");			
+		
+		this.setIcon(d);								
+		this.setRolloverIcon(p);
+		this.setPressedIcon(d);
+		this.setContentAreaFilled(false);			
+		this.setBorderPainted(false);
+		
+	}
+}
