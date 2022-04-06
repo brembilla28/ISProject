93c20c525847032d35145252ebabfc30b277867f
brembilla28 <50902847+brembilla28@users.noreply.github.com>
2022-04-06 02:43:19 -0700

    update ball

--- src/Ball.java
+++ src/Ball.java
@@ -1,11 +1,18 @@
+package classi;
+
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
 public class Ball{
 	
-	private double x, y, dx, dy; 
+	//Informazioni pallina
+	
+	private double x, y, dx, dy; //posizione e velocità
 	
 	public Ball(double x, double y){
+		
+		//posiziona pallina
 		
 		this.x = x;
 		this.y = y;
@@ -14,44 +21,53 @@ public class Ball{
 	
 	public void move(){
 		
+		//movimento pallina
+		
 		this.setX(this.getX() + this.getDX());
         this.setY(this.getY() + this.getDY());
 	}
 	
+	//ottenimento immagine pallina
 	public Image getImage(){
 		
 		return new ImageIcon("ball.png").getImage();
 	}
-
+    //ottenere velocità x
     public double getDX() {
         return dx;
     }
 
+    //ottenere velocità y
     public double getDY() {
         return dy;
     }
 
-
+    //ottenere coordinata x
     public double getX() {
         return x;
     }
 
+  //ottenere coordinata y
     public double getY() {
         return y;
     }
     
+    //impostazione velocità x
     public void setDX(double dx) {
         this.dx = dx;
     }
     
+    //impostazione velocità x
     public void setDY(double dy) {
         this.dy = dy;
     }
     
+    //impostazione coordinata x
     public void setX(double x) {
         this.x = x;
     }
 
+    //impostazione coordinata x
     public void setY(double y) {
         this.y = y;
     }
